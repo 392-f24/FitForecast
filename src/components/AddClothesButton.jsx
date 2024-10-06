748327bec450583599c73d738003aa8e19535622
Isa Gonzalez <isaegonzalez03@gmail.com>
2024-10-06 14:18:15 -0500

    Fixed styling issues

--- src/components/AddClothesButton.jsx
+++ src/components/AddClothesButton.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable react/prop-types */
-const AddClothesButton = ({onClick}) => {
+const AddClothesButton = ({ onClick }) => {
   return (
     <button
-          className="bg-pink-500 text-white active:bg-pink-600 absolute right-10 bottom-10 z-50 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-          type="button"
-          onClick={onClick}
-        >
-          Add
+      className="bg-neutral-800 text-white absolute right-4 bottom-4 z-10 font-bold uppercase text-sm w-16 h-16 rounded-full outline-none ease-linear transition-all duration-150 flex justify-center items-center"
+      type="button"
+      onClick={onClick}
+    >
+      <span className="material-symbols-rounded text-2xl">add</span>
     </button>
-  )
-}
+  );
+};
 
-export default AddClothesButton
+export default AddClothesButton;
