fabbb99e9f8bf2fdceab28c07e2a6bbe92110bee
Suhuai <suhuaichen2024@u.northwestern.edu>
2024-10-06 13:41:49 -0500

    Adjust button position

--- src/components/AddClothesButton.jsx
+++ src/components/AddClothesButton.jsx
@@ -2,7 +2,7 @@
 const AddClothesButton = ({onClick}) => {
   return (
     <button
-          className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+          className="bg-pink-500 text-white active:bg-pink-600 absolute right-10 bottom-10 z-50 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
           type="button"
           onClick={onClick}
         >
