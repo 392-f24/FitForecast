c9e4c5a27219e85bf3277a5d9d5dac8d5a2fa2d8
Aining Li <annieli2025@u.northwestern.edu>
2024-10-11 00:41:18 -0500

    update closet cloth categories and import icons

--- src/pages/Closet.jsx
+++ src/pages/Closet.jsx
@@ -1,50 +1,108 @@
-import { useState } from 'react';
+import { useState } from "react";
 import AddClothesButton from "../components/AddClothesButton";
+import AllIcon from "../assets/all.svg";
 import EditForm from "../components/EditForm";
-import ShirtSVG from '../assets/test.svg';
-import PantSVG from '../assets/jeans.svg';
-import BagSVG from '../assets/bag.svg';
-import ShoesSVG from '../assets/shoesTest.svg';
-import AllIcon from '../assets/all.svg';
-import ShirtsIcon from '../assets/shirts.svg';
-import PantsIcon from '../assets/pants.svg';
-import ShoesIcon from '../assets/shoes.svg';
-import JacketsIcon from '../assets/jackets.svg';
-import AccessoriesIcon from '../assets/accessories.svg';
 
-function Closet() {     
+// import ShirtSVG from "../assets/test.svg";
+// import PantSVG from "../assets/jeans.svg";
+import BagSVG from "../assets/bag.svg";
+// import ShoesSVG from "../assets/shoesTest.svg";
+
+// import ShirtsIcon from "../assets/shirts.svg";
+// import PantsIcon from "../assets/pants.svg";
+// import ShoesIcon from "../assets/shoes.svg";
+// import JacketsIcon from "../assets/jackets.svg";
+// import AccessoriesIcon from "../assets/accessories.svg";
+
+// import clothes icons
+import HoodieIcon from "../assets/icons/hoodie.svg";
+import BootsIcon from "../assets/icons/boots.svg";
+import DressIcon from "../assets/icons/dress.svg";
+import DressShoesIcon from "../assets/icons/dressshoes.svg";
+import LongSleeveIcon from "../assets/icons/longsleeve.svg";
+import PantsIcon from "../assets/icons/pants.svg";
+import SandalsIcon from "../assets/icons/sandals.svg";
+import ShortsIcon from "../assets/icons/shorts.svg";
+import SneakersIcon from "../assets/icons/sneakers.svg";
+import TShirtIcon from "../assets/icons/t-shirt.svg";
+import ShirtIcon from "../assets/icons/shirt.svg";
+import SweaterIcon from "../assets/icons/sweater.svg";
+import JacketIcon from "../assets/icons/jacket.svg";
+import RainJacketIcon from "../assets/icons/rainjacket.svg";
+import CoatIcon from "../assets/icons/coat.svg";
+
+function Closet() {
   const [showModal, setShowModal] = useState(false);
-            
-  const [selectedCategory, setSelectedCategory] = useState('All');
 
-  const categories = ["All", "Shirts", "Pants", "Shoes", "Jackets", "Accessories"];
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  // const categories = [
+  //   "All",
+  //   "Shirts",
+  //   "Pants",
+  //   "Shoes",
+  //   "Jackets",
+  //   "Accessories",
+  // ];
+
+  const categories = [
+    "All",
+    "TShirt",
+    "Shirt",
+    "LongSleeve",
+    "Dress",
+    "Shorts",
+    "Pants",
+    "Sandals",
+    "Sneakers",
+    "Boots",
+    "DressShoes",
+    "Sweater",
+    "Hoodie",
+    "Jacket",
+    "RainJacket",
+    "Coat",
+  ];
 
   // Map categories to their icons
   const categoryIcons = {
     All: AllIcon,
-    Shirts: ShirtsIcon,
+    TShirt: TShirtIcon,
+    Shirt: ShirtIcon,
+    LongSleeve: LongSleeveIcon,
+    Dress: DressIcon,
+    Shorts: ShortsIcon,
     Pants: PantsIcon,
-    Shoes: ShoesIcon,
-    Jackets: JacketsIcon,
-    Accessories: AccessoriesIcon,
+    Sandals: SandalsIcon,
+    Sneakers: SneakersIcon,
+    Boots: BootsIcon,
+    DressShoes: DressShoesIcon,
+    Sweater: SweaterIcon,
+    Hoodie: HoodieIcon,
+    Jacket: JacketIcon,
+    RainJacket: RainJacketIcon,
+    Coat: CoatIcon,
   };
+
   const clothes = [
     // Populate this array with actual clothing items with a category property
-    { imageUrl: ShirtSVG, category: 'Shirts' },
-    { imageUrl: PantSVG, category: 'Pants' },
-    { imageUrl: ShoesSVG, category: 'Shoes' },
-    { imageUrl: ShirtSVG, category: 'Jackets' },
-    { imageUrl: BagSVG, category: 'Accessories' },
+    // { imageUrl: ShirtSVG, category: "Shirts" },
+    // { imageUrl: PantSVG, category: "Pants" },
+    // { imageUrl: ShoesSVG, category: "Shoes" },
+    // { imageUrl: ShirtSVG, category: "Jackets" },
+    // { imageUrl: BagSVG, category: "Accessories" },
+    { imageUrl: BagSVG, category: "Hoodie" },
   ];
 
-  const filteredClothes = selectedCategory === 'All' 
-    ? clothes 
-    : clothes.filter(item => item.category === selectedCategory);
+  const filteredClothes =
+    selectedCategory === "All"
+      ? clothes
+      : clothes.filter((item) => item.category === selectedCategory);
 
   return (
     <div className="p-4">
-      <EditForm showModal={showModal} setShowModal={setShowModal}/>
-    
+      <EditForm showModal={showModal} setShowModal={setShowModal} />
+
       {/* Category icons */}
       <div className="flex space-x-4 overflow-x-auto mb-4">
         {categories.map((category, index) => (
@@ -52,7 +110,7 @@ function Closet() {
             key={index}
             onClick={() => setSelectedCategory(category)}
             className={`w-16 h-16 flex-shrink-0 flex items-center justify-center bg-white-100 rounded-full p-2 hover:bg-gray-300 transition ${
-              selectedCategory === category ? 'bg-blue-100 text-white' : ''
+              selectedCategory === category ? "bg-blue-100 text-white" : ""
             }`}
           >
             <img
@@ -68,16 +126,24 @@ function Closet() {
       <div className="h-[calc(100vh-150px)] overflow-y-auto">
         {/* Clothing grid */}
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
-          {filteredClothes.map((item, index) => (
-            <div key={index} className="flex items-center justify-center bg-gray-100 p-4 rounded-md">
-              {/* Use SVG or image for each clothing item */}
-              <img src={item.imageUrl} alt={`Clothing item ${index + 1}`} className="w-full h-auto" />
+          <p>... Need to fetch clothes data from database</p>
+          {/* {filteredClothes.map((item, index) => (
+            <div
+              key={index}
+              className="flex items-center justify-center bg-gray-100 p-4 rounded-md"
+            >
+              Use SVG or image for each clothing item
+              <img
+                src={item.imageUrl}
+                alt={`Clothing item ${index + 1}`}
+                className="w-full h-auto"
+              />
             </div>
-          ))}
+          ))} */}
         </div>
       </div>
-      
-      <AddClothesButton onClick={() => setShowModal(true)}/>
+
+      <AddClothesButton onClick={() => setShowModal(true)} />
     </div>
   );
 }
