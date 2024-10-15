b7e5022f578a3b167e2d57682b795c32cc2ba75d
charlielovett <115426211+charlielovett@users.noreply.github.com>
2024-10-15 13:46:14 -0500

    hello world retrive data

--- functions/index.js
+++ functions/index.js
@@ -21,7 +21,19 @@ const {logger} = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-exports.hello = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello World!");
+exports.hello = onRequest(async (request, response) => {
+
+  // retrieve data
+  const snapshot = await admin.database().ref('/parentCategories').once('value');
+  const data = snapshot.val();
+  response.send(`Hi, this is firebase! Parent Categories: ${data[0].name}`);
+
+  // function logic
+
+
+  // return
+
+  
+  // logger.info("Hello logs!", {structuredData: true});
+  // response.send("Hello World!");
 });
