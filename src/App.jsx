83f7222c32e7306f65f59856bbd851b0718327d2
Isa Gonzalez <isaegonzalez03@gmail.com>
2024-10-18 11:28:15 -0500

    Added logout button

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,66 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Navigate, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Link,
+} from "react-router-dom";
 import Main from "./pages/Main";
 import Closet from "./pages/Closet";
 import Header from "./components/Header";
 import Login from "./pages/Login";
 import { useState, useEffect } from "react";
-import { getAuth, onAuthStateChanged, setPersistence, browserSessionPersistence } from "firebase/auth";
+import {
+  getAuth,
+  onAuthStateChanged,
+  setPersistence,
+  browserSessionPersistence,
+} from "firebase/auth";
 
 function App() {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const auth = getAuth();
-  
+
   useEffect(() => {
-
     setPersistence(auth, browserSessionPersistence)
-    .then(() => {
+      .then(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+          if (user) {
+            setUser(user);
+          } else {
+            setUser(null);
+          }
+          setLoading(false); // Authentication check is complete
+        });
 
-      const unsubscribe = onAuthStateChanged(auth, (user) => {
-        if (user) {
-          setUser(user); 
-        } else {
-          setUser(null); 
-        }
-        setLoading(false); // Authentication check is complete
+        return () => unsubscribe();
+      })
+      .catch((error) => {
+        console.error("Failed to set persistence:", error);
       });
-
-      return () => unsubscribe();
-
-    })
-    .catch((error) => {
-      console.error("Failed to set persistence:", error);
-    });
   }, [auth]);
 
   if (loading) {
     // Show a loading spinner or message while the auth state is being checked
     return <div>Loading...</div>;
   }
-  
+
   return (
     <Router>
       <div className="flex flex-col h-screen">
-        <Header />
+        <Header user={user} />
         <main className="flex-grow mt-16">
           <Routes>
-          <Route path="/" element={user ? <Main /> : <Navigate to="/login"/>} />
-            <Route path="/closet" element={user ? <Closet /> : <Navigate to="/login" />} />
+            <Route
+              path="/"
+              element={user ? <Main /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/closet"
+              element={user ? <Closet /> : <Navigate to="/login" />}
+            />
             <Route path="/login" element={<Login />} />
           </Routes>
         </main>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,27 +1,31 @@
-import React from 'react';
-import { signInWithGoogle } from '../utilities/auth';  // Import the sign-in function from auth.js
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { signInWithGoogle } from "../utilities/auth"; // Import the sign-in function from auth.js
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleLogin = async () => {
-        const user = await signInWithGoogle();
-        if (user) {
-        // Handle successful login, e.g., redirect to main page or save user data
-        navigate('/');
-        console.log('User successfully logged in:', user);
-        } else{
-            console.log('Login failed. try again')
-        }
-    };
-
-    return (
-        <div className='login-container'>
-        <h1>Get Started</h1>
-        <button onClick={handleLogin}>Sign in with Google</button>
-        </div>
-    );
+  const handleLogin = async () => {
+    const user = await signInWithGoogle();
+    if (user) {
+      // Handle successful login, e.g., redirect to main page or save user data
+      navigate("/");
+      console.log("User successfully logged in:", user);
+    } else {
+      console.log("Login failed. try again");
     }
+  };
 
-    export default Login;
+  return (
+    <div className="login-container flex flex-col p-4 gap-8 h-full justify-center">
+      <p className="text-4xl font-semibold">Get Started</p>
+      <div>
+        <button className="bg-neutral-700 text-white" onClick={handleLogin}>
+          Sign in with Google
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
